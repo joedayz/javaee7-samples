1636d48c13a2316d58521ef4289a78633294d879
Jose Diaz <jose.diaz@joedayz.pe>
2013-11-16 17:40:01 -0500

    capa de servicio agregada

---
+++ demoweb/src/main/java/pe/joedayz/cdi/service/AbstractPersistence.java
@@ -0,0 +1,64 @@
+package pe.joedayz.cdi.service;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import pe.joedayz.cdi.model.AbstractEntity;
+
+public abstract class AbstractPersistence<T 
+		extends AbstractEntity, PK extends Number> {
+
+	private Class<T> entityClass;
+	
+	public AbstractPersistence(Class<T> entityClass) {
+		this.entityClass = entityClass;
+	}
+	
+	public T save(T e) {
+		if (e.getId() != null)
+			return getEntityManager().merge(e);
+		else {
+			getEntityManager().persist(e);
+			return e;
+		}
+	}
+
+	public void remove(T entity) {
+		getEntityManager().remove(getEntityManager().merge(entity));
+	}
+
+	public T find(PK id) {
+		return getEntityManager().find(entityClass, id);
+	}
+
+	public List<T> findAll() {
+		CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
+		cq.select(cq.from(entityClass));
+		return getEntityManager().createQuery(cq).getResultList();
+	}
+
+	public List<T> findRange(int[] range) {
+		CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
+		cq.select(cq.from(entityClass));
+		Query q = getEntityManager().createQuery(cq);
+		q.setMaxResults(range[1] - range[0]);
+		q.setFirstResult(range[0]);
+		return q.getResultList();
+	}
+
+	public int count() {
+		CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
+		Root<T> rt = cq.from(entityClass);
+		cq.select(getEntityManager().getCriteriaBuilder().count(rt));
+		Query q = getEntityManager().createQuery(cq);
+		return ((Long) q.getSingleResult()).intValue();
+	}
+	
+
+	protected abstract EntityManager getEntityManager();
+	
+}
---
+++ demoweb/src/main/java/pe/joedayz/cdi/service/ArticuloServiceEJB.java
@@ -0,0 +1,29 @@
+package pe.joedayz.cdi.service;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import pe.joedayz.cdi.model.Articulo;
+
+@Stateless
+public class ArticuloServiceEJB 
+	extends AbstractPersistence<Articulo, Long>
+	implements ArticuloService {
+	
+	@PersistenceContext
+	private EntityManager em;
+
+	public ArticuloServiceEJB() {
+		super(Articulo.class);
+		
+	}
+
+	@Override
+	protected EntityManager getEntityManager() {
+		return em;
+	}
+
+	
+
+}
