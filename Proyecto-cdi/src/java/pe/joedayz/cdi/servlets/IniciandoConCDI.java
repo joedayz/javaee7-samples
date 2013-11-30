8c60f611968fd093005d2db4b7e3d4e6929a5bbb
Jose Diaz <jose.diaz@joedayz.pe>
2013-11-30 18:38:46 -0500

    Proyecto CDI

---
+++ Proyecto-cdi/src/java/pe/joedayz/cdi/servlets/IniciandoConCDI.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pe.joedayz.cdi.servlets;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ *
+ * @author josediaz
+ */
+@WebServlet("/hello-cdi")
+public class IniciandoConCDI extends HttpServlet {
+    
+}
