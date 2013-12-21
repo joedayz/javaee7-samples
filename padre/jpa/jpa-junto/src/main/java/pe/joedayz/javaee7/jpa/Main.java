7047ed8ef32344e8994f1488cefc0de4a3c9e3f5
Jose Diaz <jose.diaz@joedayz.pe>
2013-12-21 18:27:46 -0500

    insert.sql debe estar fuera de META-INF

---
+++ padre/jpa/jpa-junto/src/main/java/pe/joedayz/javaee7/jpa/Main.java
@@ -0,0 +1,39 @@
+package pe.joedayz.javaee7.jpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// 1-Creates an instance of book
+	    Book book = new Book("H2G2", 
+	    			"The Hitchhiker's Guide to the Galaxy",
+	    			12.5F, "1-84023-742-2", 354, false);
+
+	    // 2-Obtains an entity manager and a transaction
+	    EntityManagerFactory emf = 
+	    		Persistence.createEntityManagerFactory("jpaPU");
+	    EntityManager em = emf.createEntityManager();
+
+	    // 3-Persists the book to the database
+	    EntityTransaction tx = em.getTransaction();
+	    tx.begin();
+	    em.persist(book);
+	    tx.commit();
+
+	    // 4-Executes the named query
+	    book = em.createNamedQuery("findBookH2G2", Book.class).
+	    		getSingleResult();
+
+	    System.out.println("######### " + book.getDescription());
+
+	    // 5-Closes the entity manager and the factory
+	    em.close();
+	    emf.close();
+
+	}
+
+}
